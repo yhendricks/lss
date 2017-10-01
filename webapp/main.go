c90a7ecd5fecbcc2c9ba8be4c498145db848cdd6
yhendricks <yhendricks1@gmail.com>
2017-10-01 18:18:44 +0200

    Video 25 completed

--- webapp/main.go
+++ webapp/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"net/http"
+	"html/template"
+	"log"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("src/github.com/yhendricks/lss/public")))
+	templates := populateTemplate()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]			// strip off first char
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+	http.Handle("/img/", http.FileServer(http.Dir("src/github.com/yhendricks/lss/public")))
+	http.Handle("/css/", http.FileServer(http.Dir("src/github.com/yhendricks/lss/public")))
+	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplate() *template.Template {
+	result := template.New("templates")
+	const basePath = "src/github.com/yhendricks/lss/templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
