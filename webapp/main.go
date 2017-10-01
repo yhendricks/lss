30509cf8092fca6506c004f23771e945e9dac57b
yhendricks <yhendricks1@gmail.com>
2017-10-01 21:26:55 +0200

    Video 29 completed

--- webapp/main.go
+++ webapp/main.go
@@ -4,13 +4,16 @@ import (
 	"net/http"
 	"html/template"
 	"log"
+	"os"
+	"io/ioutil"
+	//"fmt"
 )
 
 func main() {
 	templates := populateTemplate()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedFile := r.URL.Path[1:]			// strip off first char
-		t := templates.Lookup(requestedFile + ".html")
+		t := templates[requestedFile + ".html"]
 		if t != nil {
 			err := t.Execute(w, nil)
 			if err != nil {
@@ -25,9 +28,37 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 }
 
-func populateTemplate() *template.Template {
-	result := template.New("templates")
+func populateTemplate() map[string]*template.Template {
+	result := make(map[string]*template.Template)
 	const basePath = "src/github.com/yhendricks/lss/templates"
-	template.Must(result.ParseGlob(basePath + "/*.html"))
+	//const basePath = "src/github.com/yhendricks/lss/templates"
+	//fmt.Println(basePath + "/_layout.html")
+	layout := template.Must(template.ParseFiles(basePath +  "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath+"/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of the content directory: " + err.Error())
+	}
+	for _, fi := range fis {
+		f, err := os.Open(basePath+"/content/"+fi.Name())
+		if err != nil {
+			panic("Failed to open template '" + fi.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read contents from file '" + fi.Name()+ "'")
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + fi.Name()+ "'")
+		}
+		result[fi.Name()] = tmpl
+	}
 	return result
 }
